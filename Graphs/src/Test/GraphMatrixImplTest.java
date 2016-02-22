ab596c9b0c9941d8af5e1b791ff604d39c89bea4
VladislavTitov <titovvlad1997@yandex.ru>
2016-02-22 05:29:47 +0300

    'final_test'

--- Graphs/src/Test/GraphMatrixImplTest.java
+++ Graphs/src/Test/GraphMatrixImplTest.java
@@ -1,5 +1,6 @@
 package Test;
 
+import org.junit.Before;
 import org.junit.Test;
 import ru.itis.inform.GraphMatrixImpl;
 
@@ -8,15 +9,19 @@ import static org.junit.Assert.*;
 public class GraphMatrixImplTest {
     private GraphMatrixImpl graph;
 
-    @org.junit.Before
+    @Before
     public void setUp() throws Exception{
         this.graph = new GraphMatrixImpl();
-        this.graph.points = 2;
+
+        graph.addPoint();
+        graph.addPoint();
+        graph.addPoint();
+
     }
 
     @org.junit.Test(expected = IllegalArgumentException.class)
     public void testAddEdge() throws Exception{
-        graph.addEdge(4, 3 , 5);
+        graph.addEdge(1, 2 , 5);                               //тест рухнет
     }
 
 }
