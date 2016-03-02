a9638868703336d4dce9d8f131b4c9c9c3087122
VladislavTitov <titovvlad1997@yandex.ru>
2016-03-03 00:32:17 +0300

    fixed node

--- FareyOnLinkedList/src/ru/itis/inform/Node.java
+++ FareyOnLinkedList/src/ru/itis/inform/Node.java
@@ -2,32 +2,32 @@ package ru.itis.inform;
 
 public class Node<T> {
 
-    private T value;
-    private Node<T> next;
-    private Node<T> previous;
+        private T value;
+        private Node<T> next;
+        private Node<T> previous;
 
-    public Node(T value) {
-        this.value = value;
-        this.next = null;
-    }
+        public Node(T value) {
+            this.value = value;
+            this.next = null;
+        }
 
-    public void setNext(Node next) {
-        this.next  = next;
-    }
+        public void setNext(Node next) {
+            this.next  = next;
+        }
 
-    public T getValue() {
-        return value;
-    }
+        public T getValue() {
+            return value;
+        }
 
-    public Node<T> getNext() {
-        return next;
-    }
+        public Node<T> getNext() {
+            return next;
+        }
 
-    public Node<T> getPrevious() {
-        return previous;
-    }
+        public Node<T> getPrevious() {
+            return previous;
+        }
 
-    public void setPrevious(Node previous) {
-        this.previous = previous;
-    }
+        public void setPrevious(Node previous) {
+            this.previous = previous;
+        }
 }
