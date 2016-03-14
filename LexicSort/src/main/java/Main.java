d953a0687cf44f9c2e6d71786abc283662b1c3a0
VladislavTitov <titovvlad1997@yandex.ru>
2016-03-14 07:54:42 +0300

    this build work

--- LexicSort/src/main/java/Main.java
+++ LexicSort/src/main/java/Main.java
@@ -1,5 +1,11 @@
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-  
+    public static void main(String[] args) throws IOException {
+        TextReaderWriter trw = new TextReaderWriter();
+        trw.readWord();
+        trw.sorter.sortWords();
+        trw.show();
+        trw.writeWord();
     }
 }
--- LexicSort/src/main/java/TextReaderWriter.java
+++ LexicSort/src/main/java/TextReaderWriter.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
@@ -8,43 +9,62 @@ public class TextReaderWriter {
     }
 
 
+
+    TextSorter sorter = new TextSorter();
+
     DoublyLinkedList<String> listOfString = new DoublyLinkedList<String>();
-    int k;
+    int k =0;
 
-    public void readWord(){
+    public void readWord() throws IOException{
 
-        Scanner scanner = new Scanner("C:\\repo\\LexicSort\\src\\main\\resources\\Humans.txt");
+        Scanner scanner = new Scanner(new File("C:\\repo\\LexicSort\\src\\main\\resources\\Humans.txt"));
         String word;
-
-        if (scanner.hasNextInt()){
-            k = scanner.nextInt();
-        }
 
         while(scanner.hasNext()){
 
             word = scanner.next();
             listOfString.addLast(word);
+            int l = word.length();
+            if (l > k){
+                k = l;
+            }
 
         }
 
     }
 
-    class TextSort{
+    class TextSorter{
+
 
         public void sortWords(){
-            for (int m = k; k > 0; k--){
+
+            for (int m = k; m > 0; m--){
                 ArrayList<DoublyLinkedList> arrayList = new ArrayList<DoublyLinkedList>(26);
-
+                listOfString.iterator.back();
                 while (listOfString.iterator.hasNext()){
-                    int s = (int) listOfString.iterator.getNext().charAt(m) - 97;
-                    if (arrayList.get(s) == null){
-                        DoublyLinkedList<String> words = new DoublyLinkedList<String>();
-                        words.addFirst(listOfString.iterator.getNext());
-                        arrayList.add(words, s);
-                    }else{
-
+                    if(listOfString.current.getValue() != null) {
+                        int s = (int) listOfString.iterator.getNext().charAt(m - 1) - 97;
+                        if (arrayList.get(s) == null) {
+                            DoublyLinkedList<String> words = new DoublyLinkedList<String>();
+                            words.addFirst(listOfString.iterator.getNext());
+                            arrayList.add(words, s);
+                        } else {
+                            arrayList.get(s).addLast(listOfString.iterator.getNext());
+                        }
                     }
+
+                    listOfString.iterator.next();
                 }
+
+                listOfString.clear();
+                listOfString.addLast(null);
+
+                for (int i = 26; i > 0; i--){
+                    if (arrayList.get(i - 1) != null) {
+                        listOfString.append(arrayList.get(i - 1));
+                    }else continue;
+                }
+
             }
         }
 
@@ -52,9 +72,12 @@ public class TextReaderWriter {
 
     public void writeWord() throws IOException{
         FileWriter writer = new FileWriter("C:\\repo\\LexicSort\\src\\main\\resources\\SortedHumans.txt");
+        listOfString.iterator.back();
         while (listOfString.iterator.hasNext()){
-            writer.write(listOfString.iterator.getNext() + " ");
-            writer.write("\n");
+            if (listOfString.current.getValue() != null) {
+                writer.write(listOfString.iterator.getNext() + " ");
+                writer.write("\n");
+            }
             listOfString.iterator.next();
         }
         writer.flush();
