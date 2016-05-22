a2d4cf3380249a61f9779608ec7c531cae2fe059
VladislavTitov <titovvlad1997@yandex.ru>
2016-05-22 16:57:30 +0300

    print Space

--- Tokenizer/src/main/java/Main.java
+++ Tokenizer/src/main/java/Main.java
@@ -1,11 +1,8 @@
-/**
- * Created by titov on 16.05.2016.
- */
 public class Main {
 
     public static void main(String[] args) {
         Tokenizer tokenizer = new Tokenizer();
-        tokenizer.process("123, acv: 12 qw; 124252");
+        tokenizer.process("I'm Titov Vlad. I'm 18. I like to learn in KFU! How are you?");
     }
 
 }
